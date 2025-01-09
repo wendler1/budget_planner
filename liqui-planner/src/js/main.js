14f7cad7a1d165c8e6b6c3a530df7b53e1e82258
wendler1 <40688412+wendler1@users.noreply.github.com>
2025-01-09 13:48:26 +0100

    template_strings

--- liqui-planner/src/js/main.js
+++ liqui-planner/src/js/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+// Get the input from the user
+let title_1 = prompt("title:");
+console.log(title_1);
+
+let input_1 = prompt("input (Einnahme/Ausgabe):");
+console.log(input_1);
+
+let amount = prompt("amount (in Cent): ");
+console.log(amount);
+
+let date = prompt("date (yyyy-mm-dd):");
+console.log(date);
+
+// Overall balance
+let income;
+let outcome;
+let total;
