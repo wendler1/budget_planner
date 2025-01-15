7ceff8804d6e28e90085cc0b29a284b17e0c3060
wendler1 <40688412+wendler1@users.noreply.github.com>
2025-01-15 10:53:29 +0100

    clean up

--- liqui-planner/src/js/main.js
+++ liqui-planner/src/js/main.js
@@ -4,53 +4,58 @@ let income = 0;
 let outcome = 0;
 let total = 0;
 
-// Get the input from the user
-let title_1 = prompt("title:");
-let input_1 = prompt("input (income/expenses):");
-let amount_1 = parseInt(prompt("amount (in cent): "));
-let date_1 = prompt("date (yyyy-mm-dd):");
-console.log(`date: ${date_1}
-    input: ${input_1}
-    amount: ${amount_1} ct
-    date: ${date_1}`
-);
+let title,
+    input, 
+    amount, 
+    date;
 
-if (input_1 === "income") {
-    income = income + amount_1;
-    total = total + amount_1;
-} else if (input_1 === "expenses") {
-    outcome = outcome + amount_1;
-    total = total - amount_1;
-} else {
-    console.log("Please enter income or expenses");
-}
-    
 
-let title_2 = prompt("title:");
-let input_2 = prompt("input (income/expenses):");
-let amount_2 = parseInt(prompt("amount (in cent): "));
-let date_2 = prompt("date (yyyy-mm-dd):");
-console.log(`date: ${date_2}
-    input: ${input_2}
-    amount: ${amount_2} ct
-    date: ${date_2}`
-);
-
-if (input_2 === "income") {
-    income = income + amount_2;
-    total = total + amount_2;
-} else if (input_2 === "expenses") {
-    outcome = outcome + amount_2;
-    total = total - amount_2;
-} else {
-    console.log("Please enter income or expenses");
+// get the input from the user
+const get_information = function () {
+    title = prompt("title:");
+    input = prompt("input (income/expenses):");
+    amount = parseInt(prompt("amount (in cent): "));
+    date = prompt("date (yyyy-mm-dd):");
 }
 
-// Overall balance
-let positive = total >= 0;
+get_information();
 
-console.log(`income: ${income} ct
-    outcome: ${outcome} ct
-    total: ${total} ct
-    positive: ${positive}`
-);
+
+// show the information as output console
+const show_information = function (title,input,amount,date) {
+    console.log(`title: ${title}
+input: ${input}
+amount: ${amount} ct
+date: ${date}`
+    );
+}
+
+show_information(title,input,amount,date);
+
+
+// add the information to the total
+const add_information = function (input,amount) {
+    if (input === "income") {
+        income = income + amount;
+        total = total + amount;
+    } else if (input === "expenses") {
+        outcome = outcome + amount;
+        total = total - amount;
+    } else {
+        console.log("Please enter income or expenses");
+    }
+}
+
+add_information(input,amount);
+
+
+// show the total balance
+const show_balance = function (income,outcome,total) {
+    console.log(`income: ${income} ct
+outcome: ${outcome} ct
+total: ${total} ct
+positive: ${total >= 0}`
+    );
+}
+
+show_balance(income,outcome,total);
