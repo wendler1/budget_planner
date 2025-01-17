c87b7c1932beb5389efd4b17faf450934ff89760
wendler1 <40688412+wendler1@users.noreply.github.com>
2025-01-17 09:11:30 +0100

    objects nested and brackets

---
+++ new/objects/main.js
@@ -0,0 +1,107 @@
+"use strict";
+
+/* 
+
+1. Create an object called `account` with the following properties:
+
+objects
+
+let iban = "DE89370400440532013000";
+let bic = "COBADEFFXXX";
+let balance = 1000;
+let active = true; 
+*/
+
+let account = {
+    iban: "DE89370400440532013000",
+    bic: "COBADEFFXXX",
+    balance: 1000,
+    active: true    
+};
+
+console.log(account);
+
+// ----------------------------------------------------------------
+
+
+
+/*
+2. Create an object called `owner_1` with the following properties:
+
+nested objects
+
+*/
+
+let owner_1 = {
+    surename: "Max",
+    name: "Mustermann",
+    sex: "male",
+    age: 25    
+};
+
+let owner_2 = {
+    surename: "Maria",
+    name: "Musterfrau",
+    sex: "female",
+    age: 22
+};
+
+let account_nested = {
+    iban: "DE89370400440532013000",
+    bic: "COBADEFFXXX",
+    owner: owner_1,
+    balance: 1000,
+    active: true    
+};
+
+console.log(account_nested);
+
+// ----------------------------------------------------------------
+
+
+/*
+3. read values with dot notaion . 
+
+*/
+
+console.log(account_nested.iban);
+
+// ----------------------------------------------------------------
+
+
+/*
+4. set properties in object with dot notaion
+
+*/
+
+// change value
+account_nested.balance = 1500;
+
+// add new property
+account_nested.limit = 500;
+
+console.log(account_nested);
+
+// ----------------------------------------------------------------
+
+
+/*
+5. read values with bracket notation []
+
+*/
+
+console.log(account_nested["iban"]);
+
+// ----------------------------------------------------------------
+
+
+/* 6. set properties in object with bracket notation    
+
+*/
+
+// change value
+account_nested["balance"] = 2000;
+
+// ----------------------------------------------------------------
+
+
