4d7651f48d7126b1a82d7b7161bd6c6d9901c8bd
wendler1 <40688412+wendler1@users.noreply.github.com>
2025-01-19 12:14:36 +0100

    properties add and remove

---
+++ new/properties_add_delete/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let car = {
+    brand: "BMW",
+};
+
+// change the value of the brand property
+car.brand = "Audi";
+
+// add a new property
+car.model = "A3";
+
+// delete the brand property
+delete car.brand;
+
+console.log(car); //
